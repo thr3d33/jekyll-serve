159cf57a22098992d93fd95ba9d9e1db24b9b650
thr3d33 <>
2025-07-20 11:03:32 +0200

    updated port, cleared some echo'ed lines from entrypoint and updated README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Jekyll Serve
+Fork from Bret Fisher.
 
 Development image for jekyll with `--live_pooling`.
+
+Using:
+`ruby:3.1-slim-bullseye`
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,10 +7,6 @@ if [ ! -f Gemfile ]; then
   echo "Be sure you're in your jekyll site root and use something like this to launch"
   echo ""
   echo "docker run -p 4000:4000 -v \$(pwd):/site thr3d33/jekyll-serve"
-  echo ""
-  echo "NOTE: To create a new site, you can use the sister image thr3d33/jekyll like:"
-  echo ""
-  echo "docker run -v \$(pwd):/site thr3d33/jekyll new ."
   exit 1
 fi
 
